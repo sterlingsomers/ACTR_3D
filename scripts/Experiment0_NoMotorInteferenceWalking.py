7150177bf69558b592aaf3ff1c9faef45409eb18
Sterling Somers <sterlingsomers@gmail.com>
2015-05-26 22:46:56 -0400

    change to rotation rate. 2 degrees per

--- scripts/Experiment0_NoMotorInteferenceWalking.py
+++ scripts/Experiment0_NoMotorInteferenceWalking.py
@@ -175,11 +175,11 @@ class MotorMethods(ccm.ProductionSystem):
 
 
     def increase_rotation_abdomen_left(b_motor_command_abdomen='rotate:true direction:left', motor_module='busy:False'):
-        motor_module.increase_shoulder_rotation('left',0.02618)
+        motor_module.increase_shoulder_rotation('left',0.03491)
         #goal.set('stop')
 
     def increase_rotation_abdomen_right(b_motor_command_abdomen='rotate:true direction:right', motor_module='busy:False'):
-        motor_module.increase_shoulder_rotation('right',-0.02618)
+        motor_module.increase_shoulder_rotation('right',-0.03491)
         #goal.set('stop')
 
     def increase_rotation_shoulders(b_motor_command_shoulders='rotate:true direction:?d', motor_module='busy:False'):
