cf4e0898f3c10832cdcb0250baf8c5482b5ebafa
Sterling Somers <sterlingsomers@gmail.com>
2015-06-04 22:40:52 -0400

    half a fix for nonlinear version2

--- scripts/Exp1Runner.py
+++ scripts/Exp1Runner.py
@@ -2,6 +2,6 @@ import ccm
 import os
 
 
-ccm.run('Experiment0_NoMotorInteferenceWalking_nonLinear',50,RadiusMultiplier=[1.4],VisionMultiplier=[1.1])
+ccm.run('Experiment0_NoMotorInteferenceWalking_nonLinear',36,RadiusMultiplier=[1.4],VisionMultiplier=[1.1])
 os.remove('check.ck')
 
