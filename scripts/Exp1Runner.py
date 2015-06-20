f9d2dbf6ae72197193c79917f4486224420804ee
Sterling Somers <sterlingsomers@gmail.com>
2015-06-19 22:29:22 -0400

    added the alternate_alternate which has only bottom-up

--- scripts/Exp1Runner.py
+++ scripts/Exp1Runner.py
@@ -2,6 +2,6 @@ import ccm
 import os
 
 
-ccm.run('Experiment0_NoMotorInteferenceWalking_nonLinear_lessVisualInterference',20,RadiusMultiplier=[1.8],VisionMultiplier=[1.0])
+ccm.run('Experiment0_NoMotorInteferenceWalking_nonLinear_lessVisualInterference',5,RadiusMultiplier=[1.6,1.7,1.8,1.9,2.0,2.1,2.2],VisionMultiplier=[1.0,1.1])
 os.remove('check.ck')
 
